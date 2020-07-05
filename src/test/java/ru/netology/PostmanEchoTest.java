2599cdf93fcae23dd0f363f0f6889f65b0b8cce2
Andrey Globa <Globa.Andrey@gmail.com>
2020-07-05 22:08:10 +0300

    added files

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldTestPostRequest(){
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия ost")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"))
+        ;
+    }
+}
