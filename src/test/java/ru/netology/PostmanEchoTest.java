15abf66a275d8b7c3e4a9e66964c7d32d16e602b
Andrey Globa <Globa.Andrey@gmail.com>
2020-07-06 08:55:37 +0300

    Test pass

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -1,6 +1,5 @@
 package ru.netology;
 
-import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -8,17 +7,19 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
     @Test
-    void shouldTestPostRequest(){
+    void shouldTestPostRequest() {
         // Given - When - Then
-// Предусловия
+        // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some value") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия ost")
-// Проверки
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия ost")
+                // Проверки
+                .when()
+                .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data ", equalTo("some value"))
+                .body("data ", equalTo("some data"))
         ;
     }
 }
